a2ba190fa24d459beafce8e0fdac5556063a0ab6
Michael Sogos <michael.sogos@gurustudioweb.it>
2016-10-11 18:25:06 +0200

    Version 1.0.4 * Improved verbosity * Resolved a bug fix to correctly access to REST API on TFS on-premise

--- ReleaseNoteGenerator/ReleaseNoteGeneratorTask/ReleaseNoteGenerator.ps1
+++ ReleaseNoteGenerator/ReleaseNoteGeneratorTask/ReleaseNoteGenerator.ps1
@@ -12,7 +12,7 @@ param (
 #region "Presentation"
 Write-Verbose "===================================="
 Write-Verbose "| VSTS Build ReleaseNote Generator |"
-Write-Verbose "| - Version: 1.0.3                 |"
+Write-Verbose "| - Version: 1.0.4                 |"
 Write-Verbose "| - Author:  Michael Sogos         |"
 Write-Verbose "| - License: MIT                   |"
 Write-Verbose "===================================="
@@ -25,7 +25,7 @@ $buildDefinitionName = $env:BUILD_DEFINITIONNAME
 $buildNumber = $env:BUILD_BUILDNUMBER 
 
 $headers = @{Authorization=("Bearer {0}" -f $env:SYSTEM_ACCESSTOKEN)}
-$urlChangeSets = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)/DefaultCollection/$($env:SYSTEM_TEAMPROJECT)/_apis/build/builds/$($buildId)/changes?api-version=2"
+$urlChangeSets = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/build/builds/$($buildId)/changes?api-version=2"
 
 Write-Verbose "Build ID: $($buildId)"
 Write-Verbose "Release ID: $($releaseId)"
@@ -35,13 +35,13 @@ Write-Verbose "Output: $($outputfile)"
 
 #region "Get ChangeSets associated to build"
 Write-Host "Retrieving changesets associated to build $($buildId)." 
-Write-Verbose "ChangeSets URL: $($urlChangeSets)"
 $changeSets = ""
 Try{
     $changeSets = Invoke-RestMethod -Method Get -Uri $urlChangeSets -headers $headers -ErrorAction Stop
 }
 Catch{        
     Write-Error "REST API return [$($_.Exception.Message)] getting changesets associated to build with id $($buildId)!"
+	Write-Error "REST API URL: $($urlChangeSets)"
     Exit 1    
 }
 if($changeSets.GetType().Name -ne "PSCustomObject"){
@@ -61,6 +61,7 @@ $result += Foreach ($changeSet in $changeSets.value){
 	Write-Output "`r`n#### $("{0:dd/MM/yy HH:mm:ss}" -f [datetime]$changeSet.timestamp) - _@$($changeSet.author.displayName)_  " 
 	Write-Output "`r`n$($changeSet.message)  "
 	Write-Output "`r`n"
+    Write-Verbose "ChangeSet Comment: $($changeSet)"
 } 
 
 $result | Out-File $outputfile
--- ReleaseNoteGenerator/overview.md
+++ ReleaseNoteGenerator/overview.md
@@ -1,3 +1,13 @@
-﻿#Description
+﻿# Description  
 This extension is able to create a MarkDown file format that contains all commit comments associated to the build.  
 The file can be placed in any valid path, by default it will part of the artifacts.
+
+### How it works?  
+Essentially the extension use the agent environment variables to get an access to VSTS\TFS API service, using the same credentials supplied to the agent (in this way is impossible to stole access credentials); then with a powershell script the extension will retrieve all the commit comments and fulfill a markdown template.
+
+### What's new in v1.0.4?  
+* Fixed a bug to retrieve the correct REST API url (TFS on-premise)
+* Improved debug information
+
+### General Notes
+To activate debug verbosity add ``` -verbose ``` as parameter
